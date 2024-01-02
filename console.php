c895f3fee69563685efb71d1013cb568ef9067ba
haru <kawachigenji95@gmail.com>
2024-01-02 15:54:09 +0900

    First commit

---
+++ console.php
@@ -0,0 +1,32 @@
+<?php
+spl_autoload_extensions(".php");
+spl_autoload_register(function ($class) {
+    $class = str_replace("\\", "/", $class);
+    $file = $class . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+$commands = include "Commands/registry.php";
+
+// 第2引数は実行するコマンド
+$inputCommand = $argv[1];
+
+// PHPでそれらをインスタンス化できるすべてのコマンドクラス名を通過します。
+foreach ($commands as $commandClass) {
+    $alias = $commandClass::getAlias();
+
+    if ($inputCommand === $alias) {
+        if (in_array('--help', $argv)) {
+            fwrite(STDOUT, $commandClass::getHelp());
+            exit(0);
+        } else {
+            $command = new $commandClass();
+            $result = $command->execute();
+            exit($result);
+        }
+    }
+}
+
+fwrite(STDOUT, "Failed to run any commands\n");
